7687edc4a20e682236750628dae10bf5f0fc2556
Evan Ryan <evan.hundred@gmail.com>
2023-01-08 14:22:41 -0500

    integrate basic js

---
+++ js/index.js
@@ -0,0 +1,11 @@
+/*!
+ * Start Bootstrap - The Big Picture v5.0.5 (https://startbootstrap.com/template/the-big-picture)
+ * Copyright 2013-2022 Start Bootstrap
+ * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-the-big-picture/blob/master/LICENSE)
+ */
+// This file is intentionally blank
+// Use this file to add JavaScript to your project
+document.addEventListener("DOMContentLoaded", () => {
+  // const title = document.querySelector("h2.projects-title");
+  // title.style = "color: red;";
+});
