087194f41631a65e4befaf9e90ec65d208b6ab79
Evan Ryan <evanhundred@gmail.com>
2023-05-04 12:39:29 -0400

    integrate webpack, bug fixes

--- src/index.js
+++ src/index.js
@@ -1,13 +1,35 @@
 import _ from "lodash";
 
-function component() {
-  const element = document.createElement("div");
-  element.innerHTML = _.join(["Hello", "webpack"], " ");
+import ProjectsCard from "./projectsCard";
 
-  return element;
-}
+/*!
+ * Start Bootstrap - The Big Picture v5.0.5 (https://startbootstrap.com/template/the-big-picture)
+ * Copyright 2013-2022 Start Bootstrap
+ * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-the-big-picture/blob/master/LICENSE)
+ */
+// This file is intentionally blank
+// Use this file to add JavaScript to your project
+document.addEventListener("DOMContentLoaded", () => {
+  const projectsContainer = document.querySelector("div.projects-container");
+  const projectsTitle = document.querySelector("h2.projects-title");
+  const projectItemsContainer = document.querySelector(
+    "div.project-items-container"
+  );
+  const projectsCard = new ProjectsCard(projectItemsContainer);
+  projectsTitle.addEventListener("click", (e) =>
+    projectsCard.expandProjects(e)
+  );
+  let expanded = false;
+});
 
-document.body.appendChild(component());
+// function component() {
+//   const element = document.createElement("div");
+//   element.innerHTML = _.join(["Hello", "webpack"], " ");
+
+//   return element;
+// }
+
+// document.body.appendChild(component());
 
 // import ProjectsCard from "./projectsCard";
 
