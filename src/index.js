9688758eb7ad57a375efdcb96911bf534c74cc02
Evan Ryan <evan.hundred@gmail.com>
2023-11-21 10:19:45 -0500

    integrate projectsCard

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import _ from "lodash";
-
 import ProjectsCard from "./scripts/projectsCard";
 
 /*!
@@ -7,15 +6,14 @@ import ProjectsCard from "./scripts/projectsCard";
  * Copyright 2013-2022 Start Bootstrap
  * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-the-big-picture/blob/master/LICENSE)
  */
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+
 document.addEventListener("DOMContentLoaded", () => {
   const projectsContainer = document.querySelector("div.projects-container");
   const projectsTitle = document.querySelector("h2.projects-title");
-  const projectItemsContainer = document.querySelector(
-    "div.project-items-container"
-  );
-  const projectsCard = new ProjectsCard(projectItemsContainer);
+  // const projectItemsContainer = document.querySelector(
+  //   "div.projects-container"
+  // );
+  const projectsCard = new ProjectsCard(projectsContainer);
   projectsTitle.addEventListener("click", (e) =>
     projectsCard.expandProjects(e)
   );
--- src/scripts/projectsCard.js
+++ src/scripts/projectsCard.js
@@ -1,8 +1,9 @@
 class ProjectsCard {
   constructor(containerDiv) {
     this.containerDiv = containerDiv;
+    console.log(containerDiv);
 
-    const projects = {
+    this.projects = {
       populationTracker: {
         title: "Population Tracker",
         gitHubUrl: "https://github.com/evanhundred/population-tracker",
@@ -26,76 +27,80 @@ class ProjectsCard {
       }
     };
 
-    const makeItems = () => {
-      const container = document.createElement("div");
-      container.classList.add("project-items-content");
+    // const projectsUl = document.createElement("ul");
+    const projectsContainer = this.makeItems();
+    containerDiv.appendChild(projectsContainer);
+  }
 
-      Object.values(projects).forEach((project, idx) => {
-        const projectItem = document.createElement("div");
-        projectItem.classList.add(cssIfyName("project-item"));
-        projectItem.classList.add(cssIfyName(project.title));
+  makeItems() {
+    const container = document.createElement("div");
+    container.classList.add("project-items-content");
 
-        const h4 = document.createElement("h4");
-        h4.innerText = project.title;
-        projectItem.appendChild(h4);
-
-        const links = document.createElement("div");
-        links.classList.add("project-links");
-        projectItem.appendChild(links);
-
-        let a = document.createElement("a");
-        a.href = project.gitHubUrl;
-        a.target = "_blank";
-        a.rel = "noreferrer";
-        a.innerText = "GitHub";
-        links.appendChild(a);
-
-        let p = document.createElement("p");
-        p.innerText = "|";
-        links.appendChild(p);
-
-        a = document.createElement("a");
-        a.href = project.liveUrl;
-        a.target = "_blank";
-        a.rel = "noreferrer";
-        a.innerText = "Live";
-        links.appendChild(a);
-
-        const description = document.createElement("p");
-        description.innerText = project.description;
-        projectItem.appendChild(description);
-
-        container.appendChild(projectItem);
-      });
-
-      const cssIfyName = (string) => {
-        let cssIfied = "";
-        const symbolFilter = /[A-Za-z0-9]/;
-        for (let i = 0; i < string.length; i++) {
-          string[i].match(symbolFilter)
-            ? (cssIfied += string[i].toLowerCase())
-            : (cssIfied += "-");
-        }
-        return cssIfied;
-      };
-
-      const expandProjects = (e) => {
-        e.preventDefault();
-        if (!expanded) {
-          projectItemsContainer.appendChild(makeItems());
-
-          expanded = true;
-        } else {
-          const projectItemsContent = document.querySelector(
-            "div.project-items-content"
-          );
-          projectItemsContainer.removeChild(projectItemsContent);
-          expanded = false;
-        }
-      };
-
-      return container;
+    const cssIfyName = (string) => {
+      let cssIfied = "";
+      const symbolFilter = /[A-Za-z0-9]/;
+      for (let i = 0; i < string.length; i++) {
+        string[i].match(symbolFilter)
+          ? (cssIfied += string[i].toLowerCase())
+          : (cssIfied += "-");
+      }
+      return cssIfied;
     };
+
+    Object.values(this.projects).forEach((project, idx) => {
+      const projectItem = document.createElement("div");
+      projectItem.classList.add(cssIfyName("project-item"));
+      projectItem.classList.add(cssIfyName(project.title));
+
+      const h4 = document.createElement("h4");
+      h4.innerText = project.title;
+      projectItem.appendChild(h4);
+
+      const links = document.createElement("div");
+      links.classList.add("project-links");
+      projectItem.appendChild(links);
+
+      let a = document.createElement("a");
+      a.href = project.gitHubUrl;
+      a.target = "_blank";
+      a.rel = "noreferrer";
+      a.innerText = "GitHub";
+      links.appendChild(a);
+
+      let p = document.createElement("p");
+      p.innerText = "|";
+      links.appendChild(p);
+
+      a = document.createElement("a");
+      a.href = project.liveUrl;
+      a.target = "_blank";
+      a.rel = "noreferrer";
+      a.innerText = "Live";
+      links.appendChild(a);
+
+      const description = document.createElement("p");
+      description.innerText = project.description;
+      projectItem.appendChild(description);
+
+      container.appendChild(projectItem);
+    });
+
+    return container;
+  }
+
+  expandProjects(e) {
+    e.preventDefault();
+    if (!expanded) {
+      projectItemsContainer.appendChild(makeItems());
+
+      expanded = true;
+    } else {
+      const projectItemsContent = document.querySelector(
+        "div.project-items-content"
+      );
+      projectItemsContainer.removeChild(projectItemsContent);
+      expanded = false;
+    }
   }
 }
 
