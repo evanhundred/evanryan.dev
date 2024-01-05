f79a937d0122fc06e623cfb796c68fad76856722
Evan Ryan <107318423+evanhundred@users.noreply.github.com>
2024-01-05 13:09:54 -0500

    implement photo

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 Welcome to [evanryan.dev](https://evanryan.dev).
 
 A simple portfolio featuring vanilla JavaScript and webpack.
+Theme based on [Start Bootstrap - The Big Picture v5.0.5](https://startbootstrap.com/template/the-big-picture)
 
 ![preview](./assets/erd-site-preview.png)
--- src/scripts/AboutCard.js
+++ src/scripts/AboutCard.js
@@ -1,6 +1,15 @@
+// import headshot from "./assets/face-lo-res.jpeg";
+
 class AboutCard {
   constructor(containerDiv) {
     this.containerDiv = containerDiv;
+
+    const photoCard = document.createElement("div");
+    photoCard.id = "photo-container";
+    this.containerDiv.appendChild(photoCard);
+    let photoImg = document.createElement("img");
+    photoImg.src = "./assets/face-lo-res.jpeg";
+    photoCard.appendChild(photoImg);
 
     const resume = document.createElement("div");
     // resume.classList.add("resume-container");
@@ -14,6 +23,15 @@ class AboutCard {
     const bio = document.createElement("bio");
     bio.id = "bio-container";
     this.containerDiv.appendChild(bio);
+
+    let p = document.createElement("p");
+    p.innerText =
+      "Full Stack software engineer, open to employment and collaboration.";
+    bio.appendChild(p);
+    let p2 = document.createElement("p");
+    p2.innerText =
+      "Based in Brooklyn, NYC. Developer and musician. I specialize in React, Rails and Express. Currently studying GraphQL and Prisma.";
+    bio.appendChild(p2);
   }
 }
 
