3cd182542ee6185c11f6d0729eeffdf878e6377a
Evan Ryan <107318423+evanhundred@users.noreply.github.com>
2025-11-06 20:36:09 -0500

    rename branch

--- app.js
+++ app.js
@@ -2,10 +2,21 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (request, response) => {
-  response.send('Hello World!');
-});
-
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
+
+app.get('/', (request, response) => {
+  response.send('Hello World!');
+});
+app.post('/', (request, response) => {
+  response.send('Got a POST request');
+});
+app.put('/user', (request, response) => {
+  response.send('Got a PUT request at /user');
+});
+app.delete('/user', (request, response) => {
+  response.send('Got a DELETE request at /user');
+});
+
+app.use(express.static('public'));
