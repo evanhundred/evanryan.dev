fa4bb1236629ebfcf8b01f997d9573569e8ca1e3
Evan Ryan <107318423+evanhundred@users.noreply.github.com>
2025-11-06 19:46:07 -0500

    initialize repo

--- src/index.js
+++ src/index.js
@@ -10,35 +10,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const topSpacingInstance = new Spacer(topSpacingContainer);
 
   const projectsContainer = document.querySelector('div.projects-container');
-  // const projectsTitle = document.querySelector("h2.projects-title");
   const projectsCard = new ProjectsCard(projectsContainer);
-  // projectsTitle.addEventListener("click", (e) =>
-  //   projectsCard.expandProjects(e)
-  // );
-  // let expanded = false;
-
   const aboutDiv = document.querySelector('.about-container');
   new AboutCard(aboutDiv);
 });
-
-// -----
-// next: create expandable projects cards
-// -----
-
-// document.addEventListener("DOMContentLoaded", () => {
-//   const projectsContainer = document.querySelector("div.projects-container");
-//   const projectsTitle = document.querySelector("h2.projects-title");
-//   const projectItemsList = document.querySelector("div.project-items-list");
-//   const projectsCard = new ProjectsCard(projectItemsList);
-//   let expanded = false;
-//   projectsTitle.addEventListener("click", (e) =>
-//     projectsCard.expandProjects(e)
-//   );
-// // });
-
-// ------------
-// /* Template:
-//  * Start Bootstrap - The Big Picture v5.0.5 (https://startbootstrap.com/template/the-big-picture)
-//  * Copyright 2013-2022 Start Bootstrap
-//  * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-the-big-picture/blob/master/LICENSE)
-//  */
